0fb198585aecb591e791fe3956e3cde03ce0e543
Andesson Reis <andessonreys@gmail.com>
2025-03-22 14:48:40 -0300

    feat: redirecting to the appropriate urls

--- src/components/Sidebar/Navigation/index.tsx
+++ src/components/Sidebar/Navigation/index.tsx
@@ -12,8 +12,8 @@ export interface NavigationProps {}
 export function Navigation(props: NavigationProps) {
   return (
     <nav className="flex flex-col gap-0.5">
-      <NavItem icon={Home} title="Home" />
-      <NavItem icon={Tag} title="Ofertas" />
+      <NavItem icon={Home} title="Home" href="/dashboard" />
+      <NavItem icon={Tag} title="Ofertas" href='/dashboard/offers' />
       <NavItem icon={BarChart} title="Analytics" />
       <NavItem icon={Store} title="Minha Loja" />
     </nav>
